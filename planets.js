18cad8bbc17ded74b5adcc2f4c6e9211296678cd
OnigiriJack <jackmihalik@icloud.com>
2021-02-16 15:50:02 +0900

    Breaking code out into modules

---
+++ planets.js
@@ -0,0 +1,46 @@
+const planets = [
+    {
+        name: "mercury",
+        speed: 1.607,
+        distance_from_sun: 39
+    },
+    {
+        name: "venus",
+        speed: 1.174,
+        distance_from_sun: 72
+    },
+    {
+        name: "earth",
+        speed: 1,
+        distance_from_sun: 100
+    },
+    {
+        name: "mars",
+        speed: .8,
+        distance_from_sun: 150
+    },
+    {
+        name: "jupiter",
+        speed: .434,
+        distance_from_sun: 520
+    },
+    {
+        name: "saturn",
+        speed: .323,
+        distance_from_sun: 950
+    },
+    {
+        name: "neptune",
+        speed: .228,
+        distance_from_sun: 2000
+    },
+    {
+        name: "pluto",
+        speed: .182,
+        distance_from_sun: 3000
+    },
+]
+
+
+
+export default { planets }
