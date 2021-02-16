4dca7f28fb20f7722cbd4335a1a2304621049e1c
OnigiriJack <jackmihalik@icloud.com>
2021-02-16 17:58:26 +0900

    Added a trail to the planets

--- orbit.js
+++ orbit.js
@@ -1,59 +1,54 @@
 import planets from "./planets.js";
 
 
-let mercury = document.querySelector("#mercury");
-let venus = document.querySelector("#venus");
-let earth = document.querySelector("#earth");
-let mars = document.querySelector("#mars");
-
-let jupiter = document.querySelector("#jupiter");
-let saturn = document.querySelector("#saturn");
-let neptune = document.querySelector("#neptune");
-let pluto = document.querySelector("#pluto");
-
-let sun = document.querySelector("#sun")
-
-
-
-let center = {
-    x: sun.getBoundingClientRect().left,
-    y: sun.getBoundingClientRect().top,
-}
 let scale = 7;
-console.log(sun)
 
-
+let id = 1;
 let angle = 0;
 let lastTime = null;
 function animate(time) {
-    if (lastTime != null) angle += (time - lastTime) * 0.0001;
+    let sun = document.querySelector("#sun")
+    console.log(planets)
+    let center = {
+        x: sun.getBoundingClientRect().left,
+        y: sun.getBoundingClientRect().top,
+    }
+    if (lastTime != null) angle += (time - lastTime) * 0.0011;
     lastTime = time;
+    console.log(lastTime, id)
     planets.map(planet => {
         let planetEl = document.querySelector("#" + planet.name)
         planetEl.style.left = (Math.cos(angle * planet.speed) * planet.distance_from_sun / scale + center.x) + "px";
         planetEl.style.top = (Math.sin(angle * planet.speed) * planet.distance_from_sun / scale + center.y) + "px";
+
+        if (id > 10 && id % 5 === 0) planet.recentLocations.push({ x: planetEl.style.left, y: planetEl.style.top, id: id })
+
+
+        planet.recentLocations.slice(10)
+            .map(location => {
+                let trail = document.createElement("div");
+                trail.className = "trail";
+                trail.id = String(location.id);
+                trail.style.left = location.x
+                trail.style.top = location.y
+                let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                trail.style.backgroundColor = "#" + randomColor;
+                trail.textContent = "🦑"
+                document.body.appendChild(trail)
+            });
+        id++;
+        if (planet.recentLocations.length > 10) {
+            let dot = planet.recentLocations.shift();
+
+            let id = String(dot.id);
+            let dotDiv = document.getElementById(id);
+            if (dotDiv !== null) document.body.removeChild(dotDiv);
+        }
+
     })
+    if (time < 40000) { // Stop the animation after 2 seconds
+        window.requestAnimationFrame(animate);
+    }
 
-    // venus.style.top = (Math.sin(angle * 1.174) * 72 / scale + center[0]) + "px";
-    // venus.style.left = (Math.cos(angle * 1.174) * 72 / scale + center[1]) + "px";
-
-    // earth.style.top = (Math.sin(angle) * 100 / scale + center[0]) + "px";
-    // earth.style.left = (Math.cos(angle) * 100 / scale + center[1]) + "px";
-
-    // mars.style.top = (Math.sin(angle * .8) * 150 / scale + center[0]) + "px";
-    // mars.style.left = (Math.cos(angle * .8) * 150 / scale + center[1]) + "px";
-
-    // jupiter.style.top = (Math.sin(angle * .434) * 520 / scale + center[0]) + "px";
-    // jupiter.style.left = (Math.cos(angle * .434) * 520 / scale + center[1]) + "px";
-
-    // saturn.style.top = (Math.sin(angle * .323) * 950 / scale + center[0]) + "px";
-    // saturn.style.left = (Math.cos(angle * .323) * 950 / scale + center[1]) + "px";
-
-    // neptune.style.top = (Math.sin(angle * .228) * 2000 / scale + center[0]) + "px";
-    // neptune.style.left = (Math.cos(angle * .228) * 2000 / scale + center[1]) + "px";
-
-    // pluto.style.top = (Math.sin(angle * .182) * 3000 / scale + center[0]) + "px";
-    // pluto.style.left = (Math.cos(angle * .182) * 3000 / scale + center[1]) + "px";
-    requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
--- planets.js
+++ planets.js
@@ -2,44 +2,54 @@ const planets = [
     {
         name: "mercury",
         speed: 1.607,
-        distance_from_sun: 39
+        distance_from_sun: 200,
+        recentLocations: []
     },
     {
         name: "venus",
         speed: 1.174,
-        distance_from_sun: 72
+        distance_from_sun: 400,
+        recentLocations: []
     },
     {
         name: "earth",
         speed: 1,
-        distance_from_sun: 100
+        distance_from_sun: 600,
+        recentLocations: []
     },
     {
         name: "mars",
         speed: .8,
-        distance_from_sun: 150
+        distance_from_sun: 800,
+        recentLocations: []
     },
     {
         name: "jupiter",
         speed: .434,
-        distance_from_sun: 520
+        distance_from_sun: 1000,
+        recentLocations: []
     },
     {
         name: "saturn",
         speed: .323,
-        distance_from_sun: 950
+        distance_from_sun: 1200,
+        recentLocations: []
     },
     {
         name: "neptune",
         speed: .228,
-        distance_from_sun: 2000
+        distance_from_sun: 1400,
+        recentLocations: []
     },
     {
         name: "pluto",
         speed: .182,
-        distance_from_sun: 3000
+        distance_from_sun: 1600,
+        recentLocations: []
     },
 ]
+
+
 
 
 
